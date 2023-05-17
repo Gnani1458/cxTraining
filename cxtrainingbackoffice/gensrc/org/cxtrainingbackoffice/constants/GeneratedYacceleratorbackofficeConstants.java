e7ad17d86506efd5c772bc106d69639b3d6e30b8
Manohar_TK <manohar.reddy.external@thyssenkrupp-materials.com>
2023-05-17 08:02:18 +0530

    commiting the cxtrainingbackoffice extension

---
+++ cxtrainingbackoffice/gensrc/org/cxtrainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 16, 2023, 7:45:00 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.cxtrainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "cxtrainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
